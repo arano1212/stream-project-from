6cdf318c69716b0533a6d0b9c88d9858fdd1e588
arano1212 <arano1212@gmail.com>
2024-06-11 00:52:21 -0600

    protecciones de rutas y que mostrar si estamos login o logout

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,56 +1,97 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { useAuthContext } from '@/Hooks/useAuth'
 
 const Header = () => {
+  const { isAuth, logout } = useAuthContext()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logout()
+    navigate('/login')
+  }
+
   return (
     <>
-      <>
-        <nav className='navbar navbar-expand-lg bg-dark' data-bs-theme='dark'>
-          <div className='container-fluid'>
-            <NavLink className='navbar-brand' to='/welcome'> <img
+      <nav className='navbar navbar-expand-lg bg-dark' data-bs-theme='dark'>
+        <div className='container-fluid'>
+          <NavLink className='navbar-brand' to='/welcome'>
+            <img
               className='mb-4'
               src='./src/assets/s.png'
               width={45}
               height={40}
               alt='logo-stream'
               style={{ marginLeft: '10px', marginRight: '25px' }}
-                                                             />Stream
-            </NavLink>
-            <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarColor02' aria-controls='navbarColor02' aria-expanded='false' aria-label='Toggle navigation'>
-              <span className='navbar-toggler-icon' />
-            </button>
-            <div className='collapse navbar-collapse' id='navbarColor02'>
-              <ul className='navbar-nav me-auto'>
-                <li className='nav-item'>
-                  <NavLink className='nav-link active' to='/'>Home
-                    <span className='visually-hidden'>(current)</span>
-                  </NavLink>
-                </li>
-                <li className='nav-item'>
-                  <NavLink className='nav-link' to='/login'>Login</NavLink>
-                </li>
-                <li className='nav-item'>
-                  <NavLink className='nav-link' to='/register'>Register</NavLink>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link' href='#'>About</a>
-                </li>
-                <li className='nav-item dropdown'>
-                  <a className='nav-link dropdown-toggle' data-bs-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>Dropdown</a>
-                  <div className='dropdown-menu'>
-                    <NavLink className='dropdown-item' to='/createmovie'>Post</NavLink>
-                    <a className='dropdown-item' href='#'>Another action</a>
-                    <a className='dropdown-item' href='#'>Something else here</a>
-                    <div className='dropdown-divider' />
-                    <a className='dropdown-item' href='#'>Separated link</a>
-                  </div>
-                </li>
-              </ul>
-
-            </div>
+            />
+            Stream
+          </NavLink>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarColor02'
+            aria-controls='navbarColor02'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon' />
+          </button>
+          <div className='collapse navbar-collapse' id='navbarColor02'>
+            <ul className='navbar-nav me-auto'>
+              {isAuth
+                ? (
+                  <>
+                    <li className='nav-item'>
+                      <NavLink className='nav-link active' to='/'>
+                        Home
+                        <span className='visually-hidden'>(current)</span>
+                      </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                      <a className='nav-link' href='#'>About</a>
+                    </li>
+                    <li className='nav-item dropdown'>
+                      <a
+                        className='nav-link dropdown-toggle'
+                        data-bs-toggle='dropdown'
+                        href='#'
+                        role='button'
+                        aria-haspopup='true'
+                        aria-expanded='false'
+                      >
+                        Dropdown
+                      </a>
+                      <div className='dropdown-menu'>
+                        <NavLink className='dropdown-item' to='/createmovie'>
+                          Post
+                        </NavLink>
+                        <a className='dropdown-item' href='#'>Another action</a>
+                        <a className='dropdown-item' href='#'>Something else here</a>
+                        <div className='dropdown-divider' />
+                        <button className='dropdown-item btn' onClick={handleLogout}>
+                          Logout
+                        </button>
+                      </div>
+                    </li>
+                  </>
+                  )
+                : (
+                  <>
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' to='/welcome'>Welcome</NavLink>
+                    </li>
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' to='/login'>Login</NavLink>
+                    </li>
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' to='/register'>Register</NavLink>
+                    </li>
+                  </>
+                  )}
+            </ul>
           </div>
-        </nav>
-      </>
-
+        </div>
+      </nav>
     </>
   )
 }
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,15 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Login, Home, Welcome, Register, CreateMovie } from '@/Pages'
+import { useAuthContext } from '@/Hooks/useAuth'
 
 const Index = () => {
+  const { isAuth } = useAuthContext()
+
   return (
     <>
       <Routes>
         <Route path='/welcome' element={<Welcome />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/' element={<Home />} />
-        <Route path='/createMovie' element={<CreateMovie />} />
+        <Route path='/' element={isAuth ? <Home /> : <Navigate to='/login' />} />
+        <Route path='/createMovie' element={isAuth ? <CreateMovie /> : <Navigate to='/login' />} />
       </Routes>
     </>
   )
