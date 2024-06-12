c81a8079954c7f61e8d22cec8afdbb17f325d588
arano1212 <arano1212@gmail.com>
2024-06-12 02:51:15 -0600

    likes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react'
 import axiosInstance from '@/Services/movieServices'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
+import { FaThumbsUp, FaRegBell } from 'react-icons/fa'
 import '@/Styles/home.css'
+import '@/Styles/like.css'
 
 const shuffleArray = (array) => {
   const shuffledArray = [...array]
@@ -19,6 +21,7 @@ const Home = () => {
   const [featuredMovie, setFeaturedMovie] = useState(null)
   const [selectedMovie, setSelectedMovie] = useState(null)
   const [showModal, setShowModal] = useState(false)
+  const [likedMovies, setLikedMovies] = useState({})
 
   useEffect(() => {
     axiosInstance.get('/api/v1/movies', {
@@ -62,6 +65,54 @@ const Home = () => {
     setShowModal(false)
   }
 
+  const handleLike = async (movieId) => {
+    try {
+      // Verificar si el usuario ya ha dado like a esta película
+      if (likedMovies[movieId]) {
+        alert("You've already liked this movie.")
+        return
+      }
+
+      // Obtener el token de autenticación del almacenamiento local
+      const token = localStorage.getItem('token')
+
+      // Realizar la solicitud para dar like a la película
+      await axiosInstance.patch(
+        `/api/v1/movies/${movieId}`,
+        {
+          // Incrementar el vote_count en 1
+          vote_count: movies.find((movie) => movie._id === movieId).vote_count + 1
+        },
+        {
+          // Agregar el token de autenticación al encabezado
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      )
+
+      // Realizar una solicitud GET para obtener la información actualizada de la película
+      const updatedMovieResponse = await axiosInstance.get(`/api/v1/movies/vote-counts/${movieId}`)
+      const updatedMovie = updatedMovieResponse.data
+
+      // Actualizar el estado de las películas con la información actualizada
+      setMovies((prevMovies) =>
+        prevMovies.map((movie) => (movie._id === movieId ? { ...movie, vote_count: updatedMovie.vote_count } : movie))
+      )
+
+      // Actualizar el estado para reflejar que el usuario ha dado like a esta película
+      setLikedMovies((prevState) => ({
+        ...prevState,
+        [movieId]: true
+      }))
+
+      alert("You've liked the movie successfully.")
+    } catch (error) {
+      console.error('Error liking movie:', error)
+      alert('Failed to like the movie. Please try again later.')
+    }
+  }
+
   const movieRows = []
   for (let i = 0; i < shuffledMovies.length; i += 7) {
     movieRows.push(shuffledMovies.slice(i, i + 7))
@@ -89,13 +140,26 @@ const Home = () => {
           <div className={`movies-flex row-${rowIndex}`}>
             {row.length > 0
               ? (
-                  row.map(movie => (
+                  row.map((movie) => (
                     <div className='movie-card' key={movie._id} onClick={() => handleShowModal(movie)}>
                       <img
                         src={movie.poster_path.startsWith('data:image') ? movie.poster_path : `https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                         alt={movie.title}
                       />
                       <h3>{movie.title}</h3>
+                      <p>Vote: {parseInt(movie.vote_count)}</p>
+                      {likedMovies[movie._id]
+                        ? (
+                          <span className='liked-text'>
+                            Esto te gusta
+                            <FaRegBell className='icon-party-horn' />
+                          </span>
+                          )
+                        : (
+                          <button className='like-button' onClick={(e) => { e.stopPropagation(); handleLike(movie._id) }}>
+                            <FaThumbsUp className='icon-thumb' /> Like
+                          </button>
+                          )}
                     </div>
                   ))
                 )
@@ -124,7 +188,11 @@ const Home = () => {
           <p><strong>Vote Average:</strong> {selectedMovie?.vote_average}</p>
           <p><strong>Vote Count:</strong> {selectedMovie?.vote_count}</p>
         </Modal.Body>
-        <Modal.Footer style={{ backgroundColor: '#141414', borderTop: 'none' }}>
+        <Modal.Footer style={{
+          backgroundColor: '#141414',
+          borderTop: 'none'
+        }}
+        >
           <Button variant='danger' onClick={handleCloseModal}>
             Close
           </Button>
